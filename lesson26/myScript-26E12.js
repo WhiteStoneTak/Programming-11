1f324e6624a3fc4d71e1b1015f701754d51d65af
WhiteStoneTak <initz84419@yahoo.co.jp>
2025-06-02 17:40:33 -0700

    Add files via upload

---
+++ lesson26/myScript-26E12.js
@@ -0,0 +1,7 @@
+function myFunction1() {
+    document.getElementById("image").innerHTML="<img src='https://liemcomputing.ca/content/Web/00.%20RESOURCES/00-26/pic_bulbon.gif' alt='bulbon'>";
+}
+
+function myFunction2() {
+    document.getElementById("image").innerHTML="<img src='https://liemcomputing.ca/content/Web/00.%20RESOURCES/00-26/pic_bulboff.gif' alt='bulboff'>";
+}
